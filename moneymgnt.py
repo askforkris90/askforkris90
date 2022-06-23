d2d92220216c093283e4fcf7634dd0a71677ef45
askforkris90 <77594234+askforkris90@users.noreply.github.com>
2022-06-22 21:46:51 -0400

    Money
    THis is a money app for family to work out their problems with children

---
+++ moneymgnt.py
@@ -0,0 +1,13 @@
+import family as family
+
+
+def moneyloaned(args):
+    pass
+
+    function
+    var = (request)
+    [money, team] = family,
+    input("100")
+    input("50")
+    team = input("enter your money")
+    Print("workmates")
